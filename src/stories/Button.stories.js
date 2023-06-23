53523f43445957d3ab2e77a47273b4b735cdf618
Kraxus998 <kraxusmmo@gmail.com>
2023-06-23 07:23:44 -0700

    default example settings

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -2,7 +2,7 @@ import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Design System/Atoms/Button',
+  title: 'Example/Button',
   component: Button,
   tags: ['autodocs'],
   argTypes: {
